1ef3354f36a042ecc69807a35e9617002b0ad3eb
lucasbbs <lucasbbs@live.fr>
2023-06-15 17:34:47 -0300

    feat: theme config

---
+++ src/theme.ts
@@ -0,0 +1,34 @@
+export const fonts = {
+    primary: 'Newsreader, sans-serif;',
+    secondary: "Playfair Display, serif",
+}
+
+export const colors = {
+    text: {
+        lightCoral: "#f08080",
+        lightBlack: "#282828",
+        lightRed: "#c44569",
+        white: "#ffffff",
+        black: "#000000",
+        lightGray: "#d3d3d3",
+      },
+      background: {
+        primary: "#fafafa",
+        lightBlack: "#282828",
+        black: "#000000",
+        lightBlue: "#b0f8ff",
+        white: "#ffffff",
+        lightRed: "#c44569",
+        brown: "#a52a2a",
+      },
+}
+
+export const breakpoints = {
+  xss:0,
+  xs: 415,
+  sm: 576,
+  md: 800,
+  lg: 1014,
+  xl: 1300,
+  xll: 1500
+}
