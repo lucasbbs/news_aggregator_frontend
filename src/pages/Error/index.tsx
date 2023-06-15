15e141687c3372ef8d317a7efcadc7b73ae53649
lucasbbs <lucasbbs@live.fr>
2023-06-15 17:12:29 -0300

    feat: add error page

---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,11 @@
+import { StyledContainer } from "./styles";
+
+export default function ErrorPage() {
+
+
+  return (
+    <StyledContainer>
+      <h1>404 - Uh oh, something went terribly wrong 😩</h1>
+    </StyledContainer>
+  );
+}
